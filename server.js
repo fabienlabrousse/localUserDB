5d4883e8fb32726fdd90a97cc864a92df5f53247
fabienlabrousse <fabien.labrousse@la-combe-du-lion-vert.fr>
2020-11-04 14:28:05 +0100

    Changement du code pour passer vers une DB en JSON (sqlite... plz no)

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const {validateToken} = require('./token-validator');
 const {ACCESS_TOKEN_LIFE, ACCESS_TOKEN_SECRET} = require('./const');
-const sqlite3 = require('sqlite3').verbose();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const express = require("express");
+const shortid = require('shortid');
 const jwt = require('jsonwebtoken');
 const {passwordHasher} = require('./password-hasher');
 const cors = require('cors');
@@ -17,98 +19,76 @@ app.listen(HTTP_PORT, () => {
 app.use(express.json());
 app.use(cors());
 
-const sqlite = new sqlite3.Database('./user_database.db', (err) => {
-    if (err) {
-        console.error("Error opening database " + err.message);
-    }
-});
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+db.read();
 
 app.patch("/init", (req, res) => {
-    sqlite.run('CREATE TABLE users( \
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
-            firstname NVARCHAR(20)  NOT NULL,\
-            lastname NVARCHAR(20)  NOT NULL,\
-            username NVARCHAR(20),\
-            email NVARCHAR(40)\,\
-            address NVARCHAR(200)\,\
-            password NVARCHAR(64)\
-        )', (err) => {
-        if (err) {
-            console.log("Table already exists.");
-        }
-        let insert = 'INSERT INTO users (lastname, firstname, username, email,address, password) VALUES (?,?,?,?,?,?)';
-        sqlite.run(insert, ["Gordon", "Freeman", "admin", "test@test.com", "Address 1", passwordHasher("blackmesa")],
-            (err) => {
-                if (err) {
-                    console.log("Error adding admin");
-                }
-                res.send();
-            });
 
-    });
+    const admins = {
+        id: shortid.generate(),
+        firstname:"Gordon",
+        lastname: "Freeman",
+        username: "admin",
+        email: "test@test.com",
+        address: "Address 1",
+        password: passwordHasher("blackmesa")
+    };
+    db.set('users', [admins]).write();
+    res.send();
 });
 
 app.get("/users/:id", (req, res) => {
-    const id = req.params.id;
-    sqlite.get("SELECT * FROM users where user_id = ?", id, (err, row) => {
-        if (err) {
-            res.status(400).json({"error": err.message});
-            return;
-        }
-        res.status(200).json(row);
-    });
+    const requestid = req.params.id;
+    const user = db.get('users').find({id : requestid}).value();
+    if(user) {
+        res.status(200).json(user);
+        return;
+    }
+    res.status(404).send();
 });
 
 app.get("/users", (req, res) => {
-    sqlite.all("SELECT * FROM users", [], (err, rows) => {
-        if (err) {
-            res.status(400).json({"error": err.message});
-            return;
-        }
-        res.status(200).json({rows});
-    });
+    const users = db.get('users').value();
+    if(users) {
+        res.status(200).json(users);
+        return;
+    }
+    res.status(404).send();
 });
 
 app.post("/users", (req, res) => {
-    var reqBody = req.body;
-    sqlite.run("INSERT INTO users (lastname, firstname, username, email, password) VALUES (?,?,?,?,?)",
-        [reqBody.lastname, reqBody.firstname, reqBody.username, reqBody.email, passwordHasher(reqBody.password)],
-        function (err) {
-            if (err) {
-                res.status(400).json({"error": err.message});
-                return;
-            }
-            res.status(201).json({
-                "user_id": this.lastID
-            })
-        });
+    var user = req.body;
+    user.password = passwordHasher(user.password);
+    const generatedId = shortid.generate();
+    user.id = generatedId;
+    db.get('users').push(user).write();
+    res.status(200).json({createdId: generatedId});
 });
 
 app.patch("/users/:id", (req, res) => {
-    var reqBody = req.body;
-    const id = req.params.id;
-    sqlite.run(`UPDATE users SET lastname = ?, firstname = ?, username = ?, address = ?, password = ? WHERE user_id = ?`,
-        [reqBody.lastname, reqBody.firstname, reqBody.username, reqBody.address, passwordHasher(reqBody.password), id],
-        function (err) {
-            if (err) {
-                res.status(400).json({"error": err.message});
-                return;
-            }
-            res.status(200).json({updatedID: id});
-        });
+    const requestId = req.params.id;
+    const userFound = db.get('users').some(user => user.id === requestId).value();
+    if(!userFound){
+        res.status(404).send();
+        return;
+    }
+    const user = req.body;
+    user.id = requestId;
+    user.password = passwordHasher(user.password);
+    db.get('users').find({ id: requestId }).assign(user).write();
+    res.status(200).send();
 });
 
 app.delete("/users/:id", (req, res) => {
-    const deletedID = req.params.id;
-    sqlite.run(`DELETE FROM users WHERE user_id = ?`,
-        deletedID,
-        function (err) {
-            if (err) {
-                res.status(400).json({"error": err.message});
-                return;
-            }
-            res.status(200).json({deletedID: deletedID})
-        });
+    const requestId = req.params.id;
+    const userFound = db.get('users').some(user => user.id === requestId).value();
+    if(!userFound){
+        res.status(404).send();
+        return;
+    }
+    db.get('users').remove({ id: requestId }).write();
+    res.status(200).json();
 });
 
 app.get("/login", (req, res) => {
@@ -120,27 +100,21 @@ app.get("/login", (req, res) => {
     let username = req.body.username;
     let hashedPassword = passwordHasher(req.body.password);
 
-    sqlite.get("SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1",
-        [username, hashedPassword],
-        (err, row) => {
-            if (err || !row) {
-                console.log("not found");
-                res.status(401).send();
-                return;
-            }
+    const userFound = db.get('users').some(user => user.username === username && user.password === hashedPassword).value();
+    if(!userFound) {
+        res.status(401).send();
+        return;
+    }
 
+    //use the payload to store information about the user such as username, user role, etc.
+    let accessToken = jwt.sign({
+        username: row.username
+    }, ACCESS_TOKEN_SECRET, {
+        expiresIn: ACCESS_TOKEN_LIFE
+    });
 
-            //use the payload to store information about the user such as username, user role, etc.
-            let accessToken = jwt.sign({
-                username: row.username
-            }, ACCESS_TOKEN_SECRET, {
-                expiresIn: ACCESS_TOKEN_LIFE
-            });
-
-            //send the access token to the client inside the header
-            res.send({ token: accessToken });
-        });
-
+    //send the access token to the client inside the header
+    res.send({ token: accessToken });
 });
 
 app.get("/secrets", validateToken, (req, res) => {
